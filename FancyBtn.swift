4e5a7886172f466cc5b8d40212cf04a6059b101d
Michael L Gueterman <mgueterman@Michaels-iMac.local>
2017-01-17 14:03:51 -0600

    Sign in UI custom classes complete

---
+++ FancyBtn.swift
@@ -0,0 +1,25 @@
+//
+//  FancyBtn.swift
+//  devslopes-social
+//
+//  Created by Michael L Gueterman on 1/17/17.
+//  Copyright © 2017 Michael L Gueterman. All rights reserved.
+//
+
+import UIKit
+
+class FancyBtn: UIButton {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
+        layer.shadowOpacity = 0.8
+        layer.shadowRadius = 5.0
+        layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
+        layer.cornerRadius = 2.0
+//        imageView?.contentMode = .scaleAspectFit
+        
+    }
+    
+}
---
+++ devslopes-social/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  devslopes-social
+//
+//  Created by Michael L Gueterman on 1/17/17.
+//  Copyright © 2017 Michael L Gueterman. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
